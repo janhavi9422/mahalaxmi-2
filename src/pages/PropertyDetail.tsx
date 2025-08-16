188a0b030b01c8463cfb1b2886b2b606426e61db
Pranavhirulkar4 <pranavhirulkar4@gmail.com>
2025-08-16 14:39:03 +0530

    version4

--- src/pages/PropertyDetail.tsx
+++ src/pages/PropertyDetail.tsx
@@ -242,27 +242,19 @@ const PropertyDetail = () => {
                    </div>
 
                                        <div className="w-full">
-                      <iframe 
-                        src={property.id === 1 
-                          ? "https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d59559.608732902445!2d79.065334!3d21.093599!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjHCsDA1JzM3LjAiTiA3OcKwMDMnNTUuMiJF!5e0!3m2!1sen!2sus!4v1755017153777!5m2!1sen!2sus"
-                          : property.id === 2
-                          ? "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d59559.60392113395!2d79.06534!3d21.093611!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bd4bfa3682c19c7%3A0x189b5f5ecdb1cf5c!2sDr.%20Babasaheb%20Ambedkar%20International%20Airport!5e0!3m2!1sen!2sus!4v1755019539253!5m2!1sen!2sus"
-                          : property.id === 3
-                          ? "https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d59585.263516019695!2d79.004582!3d21.029526!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjHCsDAxJzQ2LjMiTiA3OcKwMDAnMTYuNSJF!5e0!3m2!1sen!2sus!4v1755020236273!5m2!1sen!2sus"
-                          : property.id === 4
-                          ? "https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d59590.08679684465!2d78.978238!3d21.017459!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjHCsDAxJzAyLjkiTiA3OMKwNTgnNDEuNyJF!5e0!3m2!1sen!2sus!4v1755020460407!5m2!1sen!2sus"
-                          : property.id === 5
-                          ? "https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d59559.608732902445!2d79.065333!3d21.093599!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjHCsDA1JzM3LjAiTiA3OcKwMDMnNTUuMiJF!5e0!3m2!1sen!2sus!4v1755021193504!5m2!1sen!2sus"
-                          : "https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d59559.608732902445!2d79.065334!3d21.093599!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjHCsDA1JzM3LjAiTiA3OcKwMDMnNTUuMiJF!5e0!3m2!1sen!2sus!4v1755017153777!5m2!1sen!2sus"
-                        }
-                        width="100%" 
-                        height="450" 
-                        style={{border:0}} 
-                        allowFullScreen="" 
-                        loading="lazy" 
-                        referrerPolicy="no-referrer-when-downgrade"
-                        className="rounded-lg"
-                      ></iframe>
+                      {property.id === 1 ? (
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14898.094439565231!2d79.04255389999999!3d21.0117251!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bd4bdfb37dac643%3A0xfc0cd306d568d48f!2sJamtha%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1755335037454!5m2!1sen!2sin" width="100%" height="450" style={{border:0}} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade" className="rounded-lg"></iframe>
+                      ) : property.id === 2 ? (
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3722.8600278426256!2d79.10917599999999!3d21.078252!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjHCsDA0JzQxLjciTiA3OcKwMDYnMzMuMCJF!5e0!3m2!1sen!2sin!4v1755246675187!5m2!1sen!2sin" width="100%" height="450" style={{border:0}} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade" className="rounded-lg"></iframe>
+                      ) : property.id === 3 ? (
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d59585.263516019695!2d79.004582!3d21.029526!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjHCsDAxJzQ2LjMiTiA3OcKwMDAnMTYuNSJF!5e0!3m2!1sen!2sus!4v1755020236273!5m2!1sen!2sus" width="100%" height="450" style={{border:0}} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade" className="rounded-lg"></iframe>
+                      ) : property.id === 4 ? (
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d59590.08679684465!2d78.978238!3d21.017459!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjHCsDAxJzAyLjkiTiA3OMKwNTgnNDEuNyJF!5e0!3m2!1sen!2sus!4v1755020460407!5m2!1sen!2sus" width="100%" height="450" style={{border:0}} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade" className="rounded-lg"></iframe>
+                      ) : property.id === 5 ? (
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3724.52139019406!2d79.02347499999999!3d21.011813999999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjHCsDAwJzQyLjUiTiA3OcKwMDEnMjQuNSJF!5e0!3m2!1sen!2sin!4v1755246724743!5m2!1sen!2sin" width="100%" height="450" style={{border:0}} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade" className="rounded-lg"></iframe>
+                      ) : (
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d59559.608732902445!2d79.065334!3d21.093599!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjHCsDA1JzM3LjAiTiA3OcKwMDMnNTUuMiJF!5e0!3m2!1sen!2sus!4v1755017153777!5m2!1sen!2sus" width="100%" height="450" style={{border:0}} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade" className="rounded-lg"></iframe>
+                      )}
                     </div>
                  </div>
                </CardContent>
